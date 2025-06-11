30c123489efe55626ae766b20044bc905b9b39e6
𝓐𝓷𝓲𝓴𝓮𝓽 𝓖𝓾𝓹𝓽𝓪 <142677885+Aniket142004@users.noreply.github.com>
2025-06-11 12:16:51 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (59.70%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,34 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-         ListNode* temp =head;
-         int count =0;
-         while(temp!=NULL){
-            count ++;
-            temp =temp->next;
-         }
-         if (count ==n){
-            head=head->next;
-            return head;
-         }
-         int m = count-n+1;
-         temp = head;
-          for(int i=1;i<m-1;i++){ // we have to traverse just ager node to be deleted
-            temp = temp ->next;
-          }
-          temp->next=temp->next->next;
-          count--;
-          return head ;
+     // the brute force solution is written on the copy 
+     ListNode* slow = head ;
+      ListNode* fast = head;
+      for(int i=1;i<=n+1;i++){
+        if(fast==NULL) return head->next;
+        fast= fast->next;
+
+      } 
+      while(fast!=NULL){
+        slow =slow->next;
+        fast=fast->next;
+      }
+      slow->next =  slow->next->next;
+      return head ;
+     
         
     }
 };
