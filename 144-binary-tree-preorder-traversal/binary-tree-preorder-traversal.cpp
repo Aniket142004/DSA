52b81dd8919e722a7cda05588021d7b34a2397b1
𝓐𝓷𝓲𝓴𝓮𝓽 𝓖𝓾𝓹𝓽𝓪 <142677885+Aniket142004@users.noreply.github.com>
2025-12-13 20:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (40.86%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    void traversal(TreeNode* root, vector<int>&v){
-        if(root==NULL) return ;
-        v.push_back(root->val);
-        traversal(root->left,v);
-          traversal(root->right,v);
-
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>v;
-          traversal(root,v);
-          return v;
-       
-
+        // iterratively solving the solution using stacl
+        vector<int>ans;
+        stack<TreeNode*>st;
+        if(root!=NULL) st.push(root);
+        while(st.size()>0){
+           TreeNode* temp = st.top();
+           st.pop();
+           ans.push_back(temp->val);
+           if(temp->right!=NULL) st.push(temp->right);
+           if(temp->left!=NULL) st.push(temp->left); 
+        }
+        return ans ;
     }
 };
